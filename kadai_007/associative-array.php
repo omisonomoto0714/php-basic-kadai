e2ea0b399b200d9050984ea4e61cbf352bb7fdcd
omisonomoto0714 <159553436+omisonomoto0714@users.noreply.github.com>
2024-07-30 16:31:47 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    
+    $vegetables = ['neme' => 'onion', 'price' => 200, 'weight' => '160'];
+
+    print_r($vegetables);
+    
+    ?>
+  </p>
+</body>
+</html>
